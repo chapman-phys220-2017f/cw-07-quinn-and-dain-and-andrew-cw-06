474ba9bc4e5f5c5a0c7d867c361805167d6764d0
Quinn Gates <gates111@mail.chapman.edu>
2017-10-19 18:13:58 +0000

    completed taylor?

--- taylor_approx.py
+++ taylor_approx.py
@@ -10,10 +10,16 @@ def taylor(x,f,i,n):
     The integer n should be positive, and will indicate how many terms to keep in the Taylor expansion sum. 
     This function returns two arrays (x, fapprox), where x is the same as the input domain array, and fapprox is a new approximate function range 
     obtained by applying the Taylor formula at the domain point x[i] contained at the index i, keeping only n terms of the expansion."""
-    total = 0
-    fapprox = []
-    dx = x[i] - x[i-1]
-    for j in range(n):
-        mat = np.linalg.matrix_power(np.matrix(ac.derivative(x[0],x[-1],n)),j) @ f
-        total = total + (mat[i-1]*(dx**j))
-    
+    #total = 0
+    #dx = x[i] - x[i-1]
+    #for j in range(n):
+    #    mat = np.linalg.matrix_power(np.matrix(ac.derivative(x[0],x[-1],n)),j) @ f
+    #    total = total + (mat[i-1]*(dx**j))
+    def tay(e):
+        total = 0
+        for j in range(n):
+            mat = np.linalg.matrix_power(np.matrix(ac.derivative(x[0],x[-1],n)),j) @ f
+            total = total + (mat[i]*((x[e]-x[i])**j)/np.factorial(j))
+        return total
+    fx = np.vectorize(tay)
+    return (x,fx(x))
